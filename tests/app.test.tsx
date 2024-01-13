8c7dfe8f9d3fa6ec9b27c4fc0305a31b364aee8b
julinuzzo19 <julinuzzo19@gmail.com>
2024-01-13 19:55:59 -0300

    added react-testing-library and vitest

---
+++ tests/app.test.tsx
@@ -0,0 +1,15 @@
+import { describe, it, expect } from "vitest";
+import { render, screen } from "@testing-library/react";
+import App from "../src/App";
+
+describe("something truthy and falsy", () => {
+  it("true to be true", () => {
+    expect(true).toBe(true);
+  });
+
+  it("render app component contains hello world!", () => {
+    render(<App />);
+
+    expect(screen.getByText("Hola"));
+  });
+});
